312cc8e15e0c692fe18d24b1c394817bde61aa12
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-13 17:48:01 +0100

    Allenamento OIS2020

--- bin/cannons.cpp
+++ bin/cannons.cpp
@@ -1,42 +1,31 @@
-/**
- * @file cannons.cpp
- *
- * @version 01.01 2020-11-13
- *
- * @brief Allenamento OIS2020
- *
- * @ingroup cannons
- * (Note: this needs exactly one @defgroup somewhere)
- *
- * @author Castellani Davide
- *
- * Contact: contacts@castellanidavide.it
- *
+/*
+ * This template is valid both in C and in C++,
+ * so you can expand it with code from both languages.
+ * NOTE: it is recommended to use this even if you don't
+ * understand the following code.
  */
 
-// Includes
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <assert.h>
 
-// Variabiles
-int N;
+// constraints
+#define MAXN 1000000
 
-// Main code
-int main()
-{
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+// input data
+int N, i;
+int T[MAXN];
 
-  // Input
-  cin >> N;
+int main() {
+//  uncomment the following lines if you want to read/write from files
+//  freopen("input.txt", "r", stdin);
+//  freopen("output.txt", "w", stdout);
 
-  // Code
-  // ...
+    assert(1 == scanf("%d", &N));
+    for(i=0; i<N; i++)
+        assert(1 == scanf("%d", &T[i]));
 
-  // Output
-  cout << N << endl;
+    // insert your code here
 
-  // End
-  return 0;
+    printf("%d\n", 42); // print the result
+    return 0;
 }
